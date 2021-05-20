c0157c95c5f3e5599403292702e45bd50047053c
Mark Jussayin <themarkie23@gmail.com>
2021-05-20 17:57:46 +0700

    reorganized hookhelper header into header and source

--- ue1launcher/Launcher/Headers/HookHelper.h
+++ ue1launcher/Launcher/Headers/HookHelper.h
@@ -13,68 +13,20 @@ struct NativeFunctionData {
     size_t ID;
     Native Function;
 
-    NativeFunctionData() {}
-
     NativeFunctionData( size_t id, Native function ) {
         ID = id;
         Function = function;
     }
 };
 
-void AddNativeFunction( size_t id, Native function ) {
-        GNatives[id] = function;
-    }
-
-void AddNativeFunctions( NativeFunctionData data[], size_t count ) {
-    for ( size_t i = 0; i < count; i++ ) {
-        GNatives[data[i].ID] = data[i].Function;
-    }
-}
-
-// PreRenderWindows hook
-#include "RootWindowOverride.h"
-
-// Dynamic Array support
-#include "DynamicArray.h"
-
-// UnrealScript Utilities
-#include "UnrealScriptUtilities.h"
+void AddNativeFunction( size_t id, Native function );
+void AddNativeFunctions( NativeFunctionData data[], size_t count );
 
 // ==============================================
 // Redirects
 // ==============================================
 // Courtesy of Hanfling.
-void RedirectCall( void* Address, void* JumpAddress ) {
-    guard( RedirectCall );
-
-    struct FRedirectCode {
-        BYTE  _Pad[3];
-        BYTE  Jump;
-        DWORD Offset;
-    };
-
-    DWORD OldProtect;
-
-    // Make function start writeable.
-    verify( VirtualProtect( ( LPVOID ) Address, 5, PAGE_EXECUTE_READWRITE, &OldProtect ) );
-
-    // Create replacement code.
-    FRedirectCode RedirectCode;
-
-    RedirectCode.Jump = 0xE9;
-    RedirectCode.Offset = ( DWORD ) JumpAddress - ( DWORD ) Address - 5;
-
-    // Copy in replacement.
-    appMemcpy( Address, ( ( BYTE* ) &RedirectCode ) + 3, 5 );
-
-    // Restore old protection.
-    verify( VirtualProtect( ( LPVOID ) Address, 5, OldProtect, &OldProtect ) );
-
-    // https://docs.microsoft.com/en-us/windows/desktop/api/processthreadsapi/nf-processthreadsapi-flushinstructioncache
-    FlushInstructionCache( GetCurrentProcess(), NULL, 0 );
-
-    unguard;
-}
+void RedirectCall( void* Address, void* JumpAddress );
 
 // Courtesy of Hanfling.
 #define SUBSTITUTE_FUNCTION( pkg, exp, cls, func, retval, parms ) \
